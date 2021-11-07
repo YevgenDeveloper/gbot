c02c2dce2f94277ec6dab3aa6d49a50057cbd102
Yevgen <eugen.developer.it@gmail.com>
2021-11-07 13:18:22 +0000

    no message

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 pm2 stop guestabot
-git reset guestabot.js --hard
+git checkout guestabot.js
+git checkout config.json
+git checkout package.json
 git pull
+npm i
 export LASTPULLID=$(git log --format="%H" -n 1) | awk '{print substr($0,0,5)}'
 sed -i "1 i\var lastID=\"$LASTPULLID\";" guestabot.js
 pm2 restart guestabot
