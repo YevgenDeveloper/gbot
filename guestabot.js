e7b6870519f0a39057c785aa915c0b3b17898cfc
Yevgen <eugen.developer.it@gmail.com>
2021-12-14 15:59:22 +0000

    Add risibank

--- guestabot.js
+++ guestabot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Risibank = require('risibank');
+var rb = new Risibank.RisiBank();
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 const config = require("./config.json");
@@ -8,11 +10,16 @@ var db = new loki('risibank.db', {
     autoload: true,
     autoloadCallback: databaseInitialize,
     autosave: true,
-    autosaveInterval: 4000
+    autosaveInterval: 60000
 });
-var guestabank = db.addCollection('guestabank');
 var prefix = config.prefix;
+var prefixSize = prefix.length;
 var admin_role_name = config.admin_role_name
+var risibank_show_tags = config.show_risitags;
+var guestabank = db.getCollection("guestabank");
+if (guestabank === null) {
+    guestabank = db.addCollection("guestabank");
+}
 client.on('ready', () => {
     console.log(`${client.user.tag} has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     client.user.setActivity(`${prefix} - Propage la bonne parole sur ${client.guilds.size} serveurs`);
@@ -40,63 +47,48 @@ client.on('message', async msg => {
         if (command.startsWith('ping')) {
             msg.reply('Pong!');
         }
-        if (command.startsWith('bank')) {
-            subcommand = command.slice(5);
-            if (subcommand.startsWith('add')) {
-                var params = subcommand.slice(4);
-                var pic_url_idx = params.indexOf(' ');
-                var url = params.substr(0, pic_url_idx);
-                var keywords = params.substr(pic_url_idx + 1);
-                if (config.debug) {
-                    msg.reply('1 : ' + url + ' - 2 : ' + keywords);
-                }
-                removeCaller(msg, 'bank add');
-                var db_results = guestabank.find({'url': {'$contains': url}});
-                if (db_results.length == 0) {
-                    guestabank.insert({
-                        url: url,
-                        keywords: keywords
-                    });
-                    var records = guestabank.data.length;
-                    msg.reply(`tu sais, je pratique la MMA depuis 6 ans et possède pas loins de ${records} images !`)
-                } else {
-                    msg.reply("Bien tenté batard, mais un autré clé l'a posté avant toi. Ghostfag va :ghost: ")
-                }
-            } else {
-                var db_results = guestabank.find({'keywords': {'$contains': subcommand}});
-                removeCaller(msg, 'bank remove');
-                if (!config.debug) {
-                    subcommand = '';
-                }
-                if (db_results.length == 0) {
-                    msg.reply("Hmm, aucun résultat. C'est vraiment pas de CHANCE ¯\\_(ツ)_/¯");
-                } else {
-                    msg.channel.send(subcommand, {
-                        file: db_results[0].url
-                    });
-                }
+        if (command.startsWith('risibank') || command.startsWith('risitas')) {
+            params = command.slice(prefixSize + 8);
+            if(command.startsWith('risitas')) {
+                params = command.slice(prefixSize + 7);
             }
-        }
-        if (command.startsWith('risibank')) {
-            params = command.slice(9);
-            risibankUrl = getRisibankRelated(params);
             removeCaller(msg, 'risibank');
-            msg.channel.send('', {
-                file: risibankUrl
-            });
-        }
-        if (command.startsWith('prefix')) {
-            msg.reply('Current prefix is *' + prefix + '*. To change it, please type `' + prefix + 'setprefix X` where X is the prefix');
-        }
-        if (command.startsWith('setprefix')) {
-            params = command.slice(10);
-            prefix = params;
-            msg.reply('New prefix setted. Is now ' + prefix);
-            console.log('prefix updated');
+            var search = rb.searchStickers(params);
+            search.then(function(data) {
+                if(data[Object.keys(data)[0]] == undefined) {
+                    msg.channel.send("J'ai pas trouvé de de sticker correspondant à " + params, {
+                        file: 'http:
+                    });
+                } else {
+                    if(!risibank_show_tags) {
+                        params = '';
+                    }
+                    msg.channel.send('' + params, {
+                        file: data[Object.keys(data)[0]].risibank_link
+                    });
+                }
+            })
         }
         if (command.startsWith('help') || command.startsWith('aled')) {
-            msg.reply("For now, some commands are available : help, aled, prefix, setprefix, risibank");
-            msg.reply("The used prefix is " + prefix);
+            msg.reply("Commande accessible : risibank <mot clés>");
+            msg.channel.send("Commandes ADMIN : RISITAGS, LEGANGE, SETPREFIX");
+            msg.channel.send("Le préfix actuellement est " + prefix);
+        }
+        if(command.startsWith('RISITAGS') && no_access(msg)) {
+            if(risibank_show_tags) {
+                risibank_show_tags = false;
+                msg.channel.send("Ok, j'arrête de t'afficher avec les tags chelous sur la risibank :ok_hand: :grin:");
+            } else {
+                risibank_show_tags = true;
+                msg.channel.send("Ok, si t'assumes d'afficher tout tes tags chelous sur la risibank :ok_hand: :grin:");
+            }
+        }
+        if (command.startsWith('SETPREFIX') && no_access(msg)) {
+            params = command.slice(prefixSize + 9);
+            prefix = params;
+            prefixSize = prefix.length;
+            msg.reply('New prefix setted. Is now ' + prefix);
+            console.log('prefix updated');
         }
         if (command.startsWith('LEGANGE') && no_access(msg)) {
             removeCaller(msg, 'LEGANGE');
@@ -107,7 +99,10 @@ client.on('message', async msg => {
                     .catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
                 n++;
             }
-            msg.channel.send("Purifié par le GANGE :ok_hand: :grin:");
+            msg.channel.send(":ok_hand: :grin:",{
+                file: "http:
+            }
+        );
         }
     }
 });
