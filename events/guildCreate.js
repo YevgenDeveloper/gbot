39983412015555e7e8c72ce99537357bf2f65c33
Yevgen <eugen.developer.it@gmail.com>
2022-05-25 09:05:14 +0000

    Notifie ROOT des events console.log :)

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,22 @@
 module.exports = (client, guild) => {
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`${prefix} - Propage la bonne parole sur ${client.guilds.size} serveurs`);
+    client.user.setActivity(`${client.config.prefix} - Propage la bonne parole sur ${client.guilds.size} serveurs`);
+    const embed = {
+        "title": `NOUVEAU SERVEUR (${guild.name})`,
+        "color": 4886754,
+        "fields": [
+            {
+                "name": "Nom",
+                "value": guild.name
+            },
+            {
+                "name": "ID",
+                "value": guild.id
+            },
+            {
+                "name": "Utilisateurs",
+                "value": guild.memberCount
+            }
+        ]
+    };
+    client.users.get(client.config.root_user).send({embed});
 }
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,21 @@
 module.exports = (client) => {
-    console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+    const embed = {
+        "title": "BOT READY",
+        "color": 8311585,
+        "fields": [
+            {
+                "name": "Nombre de serveurs",
+                "value": client.guilds.size
+            },
+            {
+                "name": "Utilisateurs total",
+                "value": client.users.size
+            },
+            {
+                "name": "Channels total",
+                "value": client.channels.size
+            }
+        ]
+    };
+    client.users.get(client.config.root_user).send({embed});
 }
