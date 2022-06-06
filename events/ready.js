55157ae0aa52367739755558e51582e958cc2d1e
Yevgen <eugen.developer.it@gmail.com>
2022-06-06 23:46:04 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports = (client) => {
+    client.user.setActivity(`${client.config.prefix} - Propage la bonne parole sur ${client.guilds.size} serveurs - http:
     const embed = {
         "title": "BOT READY",
         "color": 8311585,
