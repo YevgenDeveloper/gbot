41e1b87b8e33b970b55f8d1d4df5bfd888136880
Yevgen <eugen.developer.it@gmail.com>
2022-07-10 02:59:20 +0000

    Bugfix

--- bot.js
+++ bot.js
@@ -41,7 +41,6 @@ fs.readdir("./commands/", (err, files) => {
         let commandName = file.split(".")[0];
         console.log(`Attempting to load command ${commandName}`);
         client.commands.set(commandName, props);
-    )
     });
 });
 client.login(config.token);
