1111f3328ddbe191535661c4aa70a9c30b99232d
Yevgen <eugen.developer.it@gmail.com>
2022-07-19 20:40:07 +0000

    Update events/message.js

--- events/message.js
+++ events/message.js
@@ -6,6 +6,9 @@ module.exports = (client, message) => {
     if(!client.guildConf.hasOwnProperty('score')) {
         client.guildConf.score = true;
     }
+    if(!client.guildConf.hasOwnProperty('nsfwOnly')) {
+        client.guildConf.nsfwOnly = true;
+    }
     if(!client.guildConf.hasOwnProperty('history')) {
         client.guildConf.history = 4;
     }
